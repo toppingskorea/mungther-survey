35fa6c379b4062bf410132b58a0c5302411d8983
chanhyukpark-29cm <chanhyuk-tech@29cm.co.kr>
2023-05-28 20:29:13 +0900

    feat: 메인 페이지 구성 완료

--- src/apps/main/MainPage.tsx
+++ src/apps/main/MainPage.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Button, Container, Heading, Input, VStack } from "@chakra-ui/react";
 
 const MainPage = () => {
-  return <div>asd</div>;
+  return (
+    <Container>
+      <VStack>
+        <Heading>123,213 명의 친구들을 만들어보세요</Heading>
+      </VStack>
+
+      <VStack>
+        <Heading>나오면 알려드릴게요.</Heading>
+        <Input placeholder="emaa@gmail.com" />
+      </VStack>
+
+      <Button>공유하기</Button>
+    </Container>
+  );
 };
 
 export default MainPage;
