407fbe64f628274599d2de37dac7056b01f74edd
chanhyukpark-29cm <chanhyuk-tech@29cm.co.kr>
2023-05-28 20:25:30 +0900

    feat: 메인 페이지 생성

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,45 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { GlobalCSS } from "@/styles";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+  type DehydratedState,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Analytics } from "@vercel/analytics/react";
+import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+
+const client = new QueryClient();
+
+const inter = Inter({ subsets: ["latin"] });
+
+const theme = extendTheme({
+  fonts: {
+    body: inter.style.fontFamily,
+    heading: inter.style.fontFamily,
+  },
+});
+
+const App = ({
+  Component,
+  pageProps,
+}: AppProps<{ dehydratedState: DehydratedState }>) => {
+  return (
+    <QueryClientProvider client={client}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ChakraProvider theme={theme}>
+          <GlobalCSS font={inter.style.fontFamily} />
+          <Analytics />
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </Hydrate>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default App;
